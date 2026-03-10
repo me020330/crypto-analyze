07790765f001c893c566e480ff72d3424a167da1
me020330 <me020330@gmail.com>
2026-03-10 14:28:08 +0200

    vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "vigenere.h"
 
+void encrypt(char *text, char *key) {
+
+    int textLen = strlen(text);
+    int keyLen = strlen(key);
+
+    for(int i = 0; i < textLen; i++) {
+
+        int p = text[i] - 'A';
+        int k = key[i % keyLen] - 'A';
+
+        char c = (p + k) % 26 + 'A';
+        text[i] = c;
+    }
+}
+
+void decrypt(char *text, char *key) {
+
+    int textLen = strlen(text);
+    int keyLen = strlen(key);
+
+    for(int i = 0; i < textLen; i++) {
+
+        int c = text[i] - 'A';
+        int k = key[i % keyLen] - 'A';
+
+        char p = (c - k + 26) % 26 + 'A';
+        text[i] = p;
+    }
+}
