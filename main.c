e78d8d0303a67765669c006dce8592dceeeb22d9
me020330 <me020330@gmail.com>
2026-03-10 14:28:34 +0200

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "vigenere.h"
 
+int main() {
+
+    char text[100] = "ATTACK";
+    char key[] = "LEMON";
+
+    printf("Original: %s\n", text);
+
+    encrypt(text, key);
+    printf("Encrypted: %s\n", text);
+
+    decrypt(text, key);
+    printf("Decrypted: %s\n", text);
+
+    return 0;
+}
